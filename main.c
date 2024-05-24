f8f263a0c6ecd84f5d6feb33c74abb0f778c94c8
unknown <TuanHung108>
2024-05-24 22:50:09 +0700

    Add calculation with +

---
+++ main.c
@@ -0,0 +1,41 @@
+    #include<stdio.h>
+
+    int main() {
+        printf("\nSIMPLE CALCULATOR");
+
+        printf("\n1. Basic Calculation");
+        printf("\n2. GCD");
+        printf("\n3. Exit");
+
+        printf("\nSelection Option [1-3]: ");
+
+
+        uint8_t option;
+        scanf("%hhu", &option);
+        printf("\nMode selected: %hhu", option);
+
+        if (option == 1) {
+            uint32_t first_number, second_number;
+            char operand;
+
+            printf("\nEnter first number: ");
+            scanf("%u", &first_number);
+
+            printf("\nEnter operand: ");
+            while (getchar() != '\n');
+            scanf("%c", &operand);
+
+            printf("\nEnter second number: ");
+            scanf("%u", &second_number);
+
+            switch (operand) {
+                case '+':
+                    printf("\n%u + %u = %u", first_number, second_number, first_number + second_number);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        return 0;    
+    }
