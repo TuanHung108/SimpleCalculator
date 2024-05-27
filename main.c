8b05bd26ae9898b4ed2e3d9eb4e4f9e9a6a07362
unknown <TuanHung108>
2024-05-27 18:53:23 +0700

    [ADD] add complete simple calculator

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
-    #include <stdio.h>
-    #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    int main() {
+int main()
+{
+    while (1)
+    {
         printf("\nSIMPLE CALCULATOR");
 
         printf("\n1. Basic Calculation");
@@ -10,12 +13,12 @@
 
         printf("\nSelection Option [1-3]: ");
 
-
         unsigned int option;
         scanf("%u", &option);
         printf("\nMode selected: %u", option);
 
-        if (option == 1) {
+        if (option == 1)
+        {
             unsigned int first_number, second_number;
             char operator;
 
@@ -29,30 +32,31 @@
             printf("\nEnter second number: ");
             scanf("%u", &second_number);
 
-            switch (operator) {
-                case '+':
-                    printf("\n%u + %u = %u", first_number, second_number, first_number + second_number);
-                    break;
-                case '-':
-                    printf("\n%u - %u = %d", first_number, second_number, first_number - second_number);
-                    break;
-                case '*':
-                    printf("\n%u * %u = %llu", first_number, second_number,(unsigned long long) first_number * second_number);
-                    break;
-                case '/':
-                    printf("\n%u / %u = %.15g", first_number, second_number,(double) first_number / second_number);
-                    break;
-                default:
-                    break;
+            switch (operator)
+            {
+            case '+':
+                printf("\n%u + %u = %u", first_number, second_number, first_number + second_number);
+                break;
+            case '-':
+                printf("\n%u - %u = %d", first_number, second_number, first_number - second_number);
+                break;
+            case '*':
+                printf("\n%u * %u = %llu", first_number, second_number, (unsigned long long)first_number * second_number);
+                break;
+            case '/':
+                printf("\n%u / %u = %.15g", first_number, second_number, (double)first_number / second_number);
+                break;
+            default:
+                break;
             }
-        } 
-        else if (option == 2) {
+        }
+        else if (option == 2)
+        {
             unsigned int first_number, second_number;
 
             printf("\nEnter first number: ");
             scanf("%u", &first_number);
             printf("First number: %u", first_number);
-
 
             printf("\nEnter second number: ");
             scanf("%u", &second_number);
@@ -60,15 +64,19 @@
 
             unsigned int gcd = (first_number < second_number) ? first_number : second_number;
 
-            for (; gcd >= 1; gcd--) {
-                if (first_number % gcd == 0 && second_number % gcd == 0) break;
+            for (; gcd >= 1; gcd--)
+            {
+                if (first_number % gcd == 0 && second_number % gcd == 0)
+                    break;
             }
 
             printf("\nGCD(%u, %u) = %u", first_number, second_number, gcd);
-        } 
-        else if (option == 3) {
+        }
+        else if (option == 3)
+        {
             printf("\nExit program!");
             exit(0);
         }
-        return 0;    
     }
+    return 0;
+}
